62f988c16c4916c9eb1ade8b22912de2aad264d2
AllTexFTW <cody.wickman@alltexsupply.com>
2024-07-23 15:50:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,49 +12,49 @@ function calculate() {
     var thrdTime = 3;
     var grvTime = 5
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == "1.5" || pSize == 2 || pSize == "1.25" || pSize == "2.5" && schedule == "STD") {
     var cutTime = 1;
     var thrdTime = 4;
     var grvTime = 1;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 3 || pSize == 4 && schedule == "STD") {
     var cutTime = 2;
     var thrdTime = 6;
     var grvTime = 5;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 5 || pSize == 6 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 6;
     var grvTime = 6;
     var totalTime = (cutTime * cuts + thrdTime * thrds + grvTime * grv)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 8 || pSize == 10 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 7;
     var grvTime = 15;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 12 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 7;
     var grvTime = 30;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 14 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 7;
     var grvTime = 10;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
 
   //Calculate time for XH pipe
@@ -63,42 +63,42 @@ function calculate() {
     var thrdTime = 3;
     var grvTime = 5
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == "1.5" || pSize == 2 || pSize == "1.25" || pSize == "2.5" && schedule == "STD") {
     var cutTime = 1;
     var thrdTime = 4;
     var grvTime = 1;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 3 || pSize == 4 && schedule == "STD") {
     var cutTime = 2;
     var thrdTime = 6;
     var grvTime = 5;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 5 || pSize == 6 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 6;
     var grvTime = 6;
     var totalTime = (cutTime * cuts + thrdTime * thrds + grvTime * grv)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 8 || pSize == 10 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 7;
     var grvTime = 15;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 12 && schedule == "STD") {
     var cutTime = 5;
     var thrdTime = 7;
     var grvTime = 30;
     var totalTime = ((cutTime * cuts + thrdTime * thrds + grvTime * grv) + buffer)
-    alert("Estimated time: " + totalTime )
+    alert("Estimated time: " + totalTime + " minutes" )
   }
   if (pSize == 14 && schedule == "STD") {
     var cutTime = 5;
